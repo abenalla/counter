fbcbc45585432dc5c8321961c1177514ffebea29
unknown <aishaben.18@gmail.com>
2020-06-22 15:37:00 -0400

    Added the increment method and attached the onClick event listener

--- src/App.js
+++ src/App.js
@@ -7,14 +7,30 @@ class Counter extends Component {
       count: 0,
     };
   }
+
+  // this is where out methods will be defined
+  increment = () => {
+    this.setState ({
+      count: this.state.count +1
+    })
+    // this is where your code goes.
+  }
+
+  decrement = () => {
+    this.setState ({
+      count: this.state.count -1
+    })
+    // this is where your code goes.
+  }
   render() {
+
     return(
     <div className = "container">
       <div className = "navbar">Counter.js</div>
       <div className = "counter">
     <h1>{this.state.count}</h1>
-        <button type ="button">Increment</button>
-        <button type ="button">Decrement</button>
+        <button type ="button" onClick={this.increment}>Increment</button>
+        <button type ="button" onClick={this.decrement}>Decrement</button>
       </div>
     </div>
     );
